020b994f7c4b1b88b872c723f24051ec672e184f
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-08-06 05:15:03 +0430

    more loaders installed + adding some css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import  App  from './App';
+import "./css1.css";
+import "./css2.css";
 
 
 const container = document.getElementById('root');
