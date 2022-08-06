a931d2eb0815b1c9ba0d95679475f4b72e659516
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-08-06 18:17:10 +0430

    env varibels added to webpack dev and webpack prod

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 const App = () => {
   return (
     <>
-      <h1>|| {process.env.NODE_ENV} ||</h1>
+      <h1>
+        || {process.env.NODE_ENV} || {process.env.NAME}
+      </h1>
       <div>hey there</div>
     </>
   );
